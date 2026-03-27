1fcbb59a32a8de386e3e5bdad11e043bc95484bf
vinceaintreadin-21 <vincentleeduriga@student.laverdad.edu.ph>
2026-03-27 14:19:37 +0800

    Done playing and updating the 07-custom-type-role.ts

--- 07-custom-type-role.ts
+++ 07-custom-type-role.ts
@@ -13,9 +13,26 @@
 
     let userRole: Role = 'admin'
 
-    // ...
+    userRole = 'student'
+    userRole = 'teacher'
 
     function access(role: Role) {
-        // ...
+        switch (role) {
+            case 'admin':
+                console.log('Full access');
+                break;
+            case 'teacher':
+                console.log('Limited access');
+                break;
+            case 'student':
+                console.log('View acccess');
+                break;
+            case 'guess':
+                console.log('Restricted access')
+                break;
+            default:
+                console.log('role unidentified')
+
+        }
     }
 })
